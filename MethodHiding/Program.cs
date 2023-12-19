efcfb62bed0a7e79eff297df1cd7901d58e10870
Syed-Narjis <154256119+Syed-Narjis@users.noreply.github.com>
2023-12-19 11:22:51 +0530

    Add files via upload

---
+++ MethodHiding/Program.cs
@@ -0,0 +1,55 @@
+﻿//Implementation of Method hiding
+using System;
+public class Employee
+{
+    public string FName;
+    public String LName;
+    public String Email;
+    //Declaring and defining method in base class
+    public void PrintFullName()                                                  
+    {
+        Console.WriteLine(FName + " " + LName);
+    }
+
+}
+//Derived class from Employee
+public class FullTimeEmployee : Employee              
+{
+   public void Etype()
+    {
+        Console.WriteLine("I am a Full time Employee");
+    }
+}
+//Derived class of Employee
+public class PartTimeEmployee : Employee                
+{
+    //hiding base class method using new 
+    public new void PrintFullName()                   
+    {
+        //Calling base class method using base
+        base.PrintFullName();                             
+        Console.WriteLine(FName + " " + LName + " - Contractor" + "      //Using new keyword");
+    }
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // creating Child class obj
+        FullTimeEmployee FT = new FullTimeEmployee();              
+        FT.FName = "Full";
+        FT.LName = "Time";
+        //Base class method will be called
+        FT.PrintFullName();
+
+        //Creating child class object
+        PartTimeEmployee PT = new PartTimeEmployee();              
+        PT.FName = "Part";
+        PT.LName = "Time";
+        //Child class method will be called
+        PT.PrintFullName();                                        
+
+    }
+
+
+}
