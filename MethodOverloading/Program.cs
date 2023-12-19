39861f466e718be9dfd2b6646628eb7c5b1ede65
Syed-Narjis <154256119+Syed-Narjis@users.noreply.github.com>
2023-12-19 11:26:26 +0530

    Add files via upload

---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,28 @@
+﻿//Implementation of Method Overloading
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        //Add method
+        Add(4, 5);
+        //Add Overloading method
+        Add(6, 7,10);                           
+    }
+    //AddMethod with 2 parameters;
+    public static void Add(int Fn,int Sn)                 
+    {
+        
+        Console.WriteLine("Sum of numbers  {0} , {1} is {2} ", Fn,Sn, Fn + Sn);
+        }
+
+    //AddMethod with 3 parameters
+    public static void Add(int Fn, int Sn, int tn)              
+    {
+        Console.WriteLine("Sum of numbers  {0} , {1} , {2} is {3} ",Fn,Sn,tn, Fn + Sn+tn);
+    }
+
+
+}
